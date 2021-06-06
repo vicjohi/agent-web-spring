97929df6d8cc088b73948c4df16540fe6292f01c
vicjohi <vicjohi@gmail.com>
2021-06-06 00:09:56 -0500

    Subiendo el proyecto

---
+++ src/main/java/com/inkadroid/api/AgentWebSpringApplication.java
@@ -0,0 +1,13 @@
+package com.inkadroid.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentWebSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentWebSpringApplication.class, args);
+	}
+
+}
